f740413be269afd923ff630171ac232d20c20f04
Phad-Kishore1998 <45930935+Phad-Kishore1998@users.noreply.github.com>
2024-06-03 15:12:46 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,35 @@
+"use strict";       //treat all JS code as newer version
+
+//alert(3 + 3 )     //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("Kishore")
+
+
+let name = "Kishore"
+let age = 25
+let isLoggedIn = false
+let state = null //example temp cannot be zero so we require null as a value.
+
+//number => 2 to power 53 (range of number)
+//bigInt we have for more higher like in trading and streaming
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefined => 
+// symbol => unique (React mai jada use hota hai)
+
+
+//object
+
+console.log(typeof "Kish")
+console.log(typeof null) // its a object
+console.log(typeof undefined) // its given as undefined as well.
+
+
+
+
+
