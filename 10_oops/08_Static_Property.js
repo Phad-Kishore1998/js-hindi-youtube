1e95e78c844e198cec1e1847fa334bbdd267553c
Phad-Kishore1998 <45930935+Phad-Kishore1998@users.noreply.github.com>
2024-06-17 17:36:11 +0000

    static and Classes

---
+++ 10_oops/08_Static_Property.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+console.log(hitesh.createId()) // Stop the use of static Id property to all the Objects 
+//instantiated by the Object from the class
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());  // Stop the use of static Id property of a different class
